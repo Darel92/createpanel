dc1767a20f685aa710b898b4a8b0fe21b71e45d8
Darel92 <darelmyid@gmail.com>
2024-12-04 12:51:09 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pterodactyl Panel Creator</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="container">
+    <h1>Create Panel Automatically in Pterodactyl</h1>
+    <form id="panelForm">
+      <label for="serverName">Server Name:</label>
+      <input type="text" id="serverName" name="serverName" required><br><br>
+      
+      <label for="userId">User ID:</label>
+      <input type="text" id="userId" name="userId" required><br><br>
+      
+      <button type="submit">Create Server</button>
+    </form>
+    <div id="response"></div>
+  </div>
+  
+  <script src="script.js"></script>
+</body>
+</html>

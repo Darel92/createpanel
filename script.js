59ab6fe6f91373e04214ec2beea96f35d56e1124
Darel92 <darelmyid@gmail.com>
2024-12-04 13:02:43 +0700

    script.js

--- script.js
+++ script.js
@@ -1,50 +1,114 @@
-document.getElementById("panelForm").addEventListener("submit", function(event) {
-  event.preventDefault();
-  
-  const serverName = document.getElementById("serverName").value;
-  const userId = document.getElementById("userId").value;
+case "1gb": {
+    if (!isPremium && !isOwner) return reply('Maaf Kamu Belum Jadi User Premium Untuk Menjadi User Premium Silahkan Beli Ke Owner Dengan Cara Ketik .owner')
 
-  const apiToken = "ptla_Fht95EM1pCyG5EGllfiqo3lrZgXv2lpUkipQgLzaho3"; // Ganti dengan token API Anda
-  const apiUrl = "https://panell.darell.my.id/api/application/servers"; // Ganti dengan URL panel Pterodactyl Anda
-  
-  const data = {
-    "name": Panel,
-    "user": gratis,
-    "egg": "27", // Ganti dengan ID egg yang sesuai
-    "docker_images": ["image_id"], // Ganti dengan ID image yang sesuai
-    "environment": {},
-    "startup": "",
-    "limits": {
-      "memory": 2048,
-      "swap": 0,
-      "disk": 5000,
-      "io": 500,
-      "cpu": 100
-    },
-    "feature_limits": {
-      "databases": 1,
-      "allocations": 1,
-      "backups": 1
-    },
-    "allocation": {
-      "default": true
-    }
-  };
+let t = text.split(',');
+if (t.length < 2) return m.reply(`*Format salah!*
+Penggunaan:
+${prefix + command} user,nomer`)
+let username = t[0];
+let u = m.quoted ? m.quoted.sender : t[1] ? t[1].replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.mentionedJid[0];
+let name = username + "1GB"
+let egg = global.eggsnya
+let loc = global.location
+let memo = "1024"
+let cpu = "10"
+let disk = "0"
+let email = username + "@darell.my.id"
+akunlo = "https://pomf2.lain.la/f/7hl5igf1.png" 
+if (!u) return
+let d = (await ananoffc.onWhatsApp(u.split`@`[0]))[0] || {}
+let password = username + "001"
+let f = await fetch(domain + "/api/application/users", {
+"method": "POST",
+"headers": {
+"Accept": "application/json",
+"Content-Type": "application/json",
+"Authorization": "Bearer " + apikey
+},
+"body": JSON.stringify({
+"email": email,
+"username": username,
+"first_name": username,
+"last_name": username,
+"language": "en",
+"password": password
+})
+})
+let data = await f.json();
+if (data.errors) return m.reply(JSON.stringify(data.errors[0], null, 2));
+let user = data.attributes
+let f2 = await fetch(domain + "/api/application/nests/5/eggs/" + egg, {
+"method": "GET",
+"headers": {
+"Accept": "application/json",
+"Content-Type": "application/json",
+"Authorization": "Bearer " + apikey
+}
+})
+m.reply(`DONE BY DarelHosting⚡
 
-  fetch(apiUrl, {
-    method: "POST",
-    headers: {
-      "Authorization": `Bearer ${apiToken}`,
-      "Accept": "Application/json",
-      "Content-Type": "Application/json"
-    },
-    body: JSON.stringify(data)
-  })
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById("response").innerText = `Server ${data.attributes.name} created successfully!`;
-  })
-  .catch(error => {
-    document.getElementById("response").innerText = "Error creating server: " + error;
-  });
-});
+ *DONE CREATE USER + SERVER ID :* ${user.id}`)
+ctf = `Hai @${u.split`@`[0]}
+
+ *Username* : ${user.username}
+ *Password* : ${password}
+ *Login* : https://enter-code.vercel.app/
+ *Kode* : s6n°¥Hu905B-#dXjD#-8/@
+
+_*NOTE :*_
+_OWNER HANYA MENGIRIM 1X DATA AKUN ANDA MOHON DI SIMPAN BAIK BAIK KALAU DATA AKUN ANDA HILANG OWNER TIDAK DAPAT MENGIRIM AKUN ANDA LAGI_
+
+DarelHosting
+`
+ananoffc.sendMessage(u,{image: {url: akunlo}, caption: ctf }, { quoted: ananoffc.chat })
+let data2 = await f2.json();
+let startup_cmd = data2.attributes.startup
+
+let f3 = await fetch(domain + "/api/application/servers", {
+"method": "POST",
+"headers": {
+"Accept": "application/json",
+"Content-Type": "application/json",
+"Authorization": "Bearer " + apikey,
+},
+"body": JSON.stringify({
+"name": name,
+"description": " ",
+"user": user.id,
+"egg": parseInt(egg),
+"docker_image": "ghcr.io/parkervcp/yolks:nodejs_18",
+"startup": startup_cmd,
+"environment": {
+"INST": "npm",
+"USER_UPLOAD": "0",
+"AUTO_UPDATE": "0",
+"CMD_RUN": "npm start"
+},
+"limits": {
+"memory": memo,
+"swap": 0,
+"disk": disk,
+"io": 500,
+"cpu": cpu
+},
+"feature_limits": {
+"databases": 5,
+"backups": 5,
+"allocations": 1
+},
+deploy: {
+locations: [parseInt(loc)],
+dedicated_ip: false,
+port_range: [],
+},
+})
+})
+let res = await f3.json()
+if (res.errors) return m.reply(JSON.stringify(res.errors[0], null, 2))
+let server = res.attributes
+let p = await m.reply(`
+DONE CUY, DATA SUDAH TERKIRIM KE NOMOR TERSEBUT ANJAY... 
+© DarelHosting
+`)        
+} 
+break
